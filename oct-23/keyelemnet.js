6ac4a8b741094467ebc951cf1e6ded7ea2217100
kishan <vishwakarmakishan620@gmail.com>
2024-10-23 11:55:26 +0530

    add

---
+++ oct-23/keyelemnet.js
@@ -0,0 +1,19 @@
+let a={
+    "id":1,
+    "name":"kishan",
+    "age":25,
+    "course":"Fsd",
+}
+a.name;
+let {name,id,age,course} = a;
+console.log(name);
+console.log(age);
+console.log(id);
+console.log(course);
+
+let b={
+    "sum":function(){
+
+    }
+}
+b.sum();
